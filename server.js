4f8bf42a7d5272dc8971f9409334d416b55dbcfa
Vandana Dalal <dalalvanu@gmail.com>
2025-05-18 19:02:29 +0530

    User Creation code added

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const connectDB = require('./config/db.js');
 
 const app = express()
 
-app.get('/', (req, res) => res.send(`API Running`))
+//connnect daatbase
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }))
+
+app.get('/', (req, res) => res.send(`API Running`));
+
+// define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+
 
 const PORT = process.env.PORT || 3000;
 
